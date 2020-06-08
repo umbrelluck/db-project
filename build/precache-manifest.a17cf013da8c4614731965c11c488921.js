51c75575559eb5d194d485af1586c1d3976a722f
umbrelluck <yarko.umbrelluck@gmail.com>
2020-06-08 13:30:08 +0300

    tr2

---
+++ build/precache-manifest.a17cf013da8c4614731965c11c488921.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38e2d3d50c40067bc2ed62175e49fe4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edb03d805cd56e4a1135",
+    "url": "/static/css/main.12a978c0.chunk.css"
+  },
+  {
+    "revision": "d5130ea1931ac1f91e12",
+    "url": "/static/js/2.59264e53.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.59264e53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edb03d805cd56e4a1135",
+    "url": "/static/js/main.df91f7a9.chunk.js"
+  },
+  {
+    "revision": "4c53e3140784a30772be",
+    "url": "/static/js/runtime-main.1ac90353.js"
+  }
+]);
